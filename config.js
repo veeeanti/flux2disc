ca255b340272501b6fd5813c5a7117159dcec210
veeλnti <113473346+veeeanti@users.noreply.github.com>
2026-05-19 01:56:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@
 
 module.exports = {
   discord: {
-    token: 'process.env.DISCORD_TOKEN',
-    serverId: 'process.env.DISCORD_SERVERID'
+    token: process.env.DISCORD_TOKEN,
+    serverId: process.env.DISCORD_SERVERID
   },
   fluxer: {
-    token: 'process.env.FLUXER_TOKEN',
-    guildId: 'process.env.FLUXER_GUILDID'
+    token: process.env.FLUXER_TOKEN,
+    guildId: process.env.FLUXER_GUILDID
   },
-  channelMapping: 'process.env.MAPPING_TYPE'
+  channelMapping: process.env.MAPPING_TYPE
 };
